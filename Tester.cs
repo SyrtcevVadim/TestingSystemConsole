5d04973eedd77ba261a63b6391bcb12358388590
SyrtcevVadim <syrtcevvi@gmail.com>
2021-04-03 19:03:46 +0300

    Tester demo class

---
+++ Tester.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace TestingSystemConsole
+{
+    /// <summary>
+    /// Класс, предназначенный для тестирования пользовательской программы на тестовых данных
+    /// </summary>
+    class Tester
+    {
+        /// <summary>
+        /// Процесс, в котором работает пользовательская программа
+        /// </summary>
+        private Process userExecutable;
+
+        private TestsReader testsReader;
+
+        private AnswersReader answersReader;
+
+        private RestrictionsReader restrictionsReader;
+
+        private void ConfigureProcess(string pathToUserExecutableFile)
+        {
+            userExecutable.StartInfo.UseShellExecute = false;
+            userExecutable.StartInfo.FileName = pathToUserExecutableFile;
+            userExecutable.StartInfo.CreateNoWindow = true;
+        }
+
+        /// <summary>
+        /// Конструктор объекта тестирующей системы
+        /// </summary>
+        /// <param name="pathToUserExecutableFile">Абсолютный или относительный путь к файлу пользовательской программы</param>
+        /// <param name="pathToTestsFile">Абсолютный или относительный путь к файлу с тестовыми данными для программы</param>
+        /// <param name="pathToAnswersFile">Абсолютный или относительный путь к файлу с эталонными ответами на тестовые данные</param>
+        /// <param name="pathToRestrictionsFile">Абсолютный или относительный путь к файлу с ограничениями на выполнение программы</param>
+        public Tester(string pathToUserExecutableFile, string pathToTestsFile, string pathToAnswersFile, string pathToRestrictionsFile ="")
+        {
+            userExecutable = new Process();
+            // Настраиваем поток для работы с пользовательской программой
+            ConfigureProcess(pathToUserExecutableFile);
+            if(pathToRestrictionsFile != "")
+            {
+                // TODO RestrictionsReader
+            }
+        }
+
+        /// <summary>
+        /// Начинает тестирование пользовательской программы на тестовых данных
+        /// </summary>
+        public void Start()
+        {
+
+        }
+
+    }
+}
--- main.cs
+++ main.cs
@@ -1,15 +1,50 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Threading;
 
 namespace TestingSystemConsole
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
+            string pathToUserExecutableFile;
+            string pathToTestsFile;
+            string pathToAnswersFile;
+            string pathToRestrictionsFile;
+            // Программа для запуска принимает 3 обязательных аргумента и 1 необязательный
+            // 1 аргумент: путь к файлу с пользовательской программой
+            // 2 аргумент: путь к файлу тестов для пользовательской программы
+            // 3 аргумент: путь к файлу эталонных ответов на входные тестовые данные
+            // 4 аргумент: путь к файлу с ограничениями
+            if (args.Length < 3)
+            {
+                Console.WriteLine("Error! Program requires 3 arguments!");
+                return 1;
+            }
+            else if(args.Length > 4)
+            {
+                Console.WriteLine("Error! Program can handle only 4 arguments!");
+            }
+            else if(args.Length == 3)
+            {
+                pathToUserExecutableFile = args[0];
+                pathToTestsFile = args[1];
+                pathToAnswersFile = args[2];
+
+
+
+            }
+            else if(args.Length == 4)
+            {
+                pathToUserExecutableFile = args[0];
+                pathToTestsFile = args[1];
+                pathToAnswersFile = args[2];
+                pathToRestrictionsFile = args[3];
+
+            }
+
+            return 0;
         }
     }
 }
